ae2baf4a09418e1dfc8c34760830f0890608c73c
Daniel Mutis <67302227+MagdiKhalifa@users.noreply.github.com>
2022-05-11 10:18:43 -0500

    Video + Actividad didáctica

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -408,8 +408,8 @@
 
     figure.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/Myko9ZdrBzM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)       
-
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/Myko9ZdrBzM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)  
+        
     .row.justify-content-center
       .col-auto
         figure
